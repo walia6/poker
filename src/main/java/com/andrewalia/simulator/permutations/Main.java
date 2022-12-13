2a59f44542a9deefe57ae26cbdeecc0283c0a881
walia6 <awalia2002@gmail.com>
2022-12-13 06:35:01 -0500

    realized combinations wasnt what i was looking for. switching to random...

--- src/main/java/com/andrewalia/simulator/permutations/Main.java
+++ src/main/java/com/andrewalia/simulator/permutations/Main.java
@@ -1,21 +1,39 @@
 package com.andrewalia.simulator.permutations;
 
 import java.util.Iterator;
+import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
 import java.util.concurrent.atomic.AtomicLong;
 
-import org.apache.commons.math3.util.Combinations;
+//import org.apache.commons.math3.util.Combinations;
 
 import com.andrewalia.simulator.permutations.thread.ComparingThread;
 import com.andrewalia.simulator.permutations.util.ShowdownResultsMap;
-import com.andrewalia.util.Deck;
+
+import it.unimi.dsi.util.SplitMix64Random;
 
 
 
 public class Main {
-    public static void main(int threadCount) throws InterruptedException {
-        final Iterator<int[]> iterator = new Combinations(Deck.FULL_DECK.size(), 9).iterator();
+    public static void main(int threadCount, int iterations) throws InterruptedException {
+        // new Combinations(Deck.FULL_DECK.size(), 9).iterator();
+        //get start time
+        double startTime = System.currentTimeMillis();
+        final Iterator<int[]> iterator = new Iterator<int[]>() {
+            private int i = 0;
+            Random random = new SplitMix64Random();
+            @Override
+            public boolean hasNext() {
+                return i++ < iterations;
+            }
+
+            @Override
+            public int[] next() {
+                return random.ints(0,52).distinct().limit(9).toArray();
+            }
+            
+        };
         final Thread[] threads = new Thread[threadCount];
         final AtomicLong permutations = new AtomicLong(0);
         final ShowdownResultsMap globalShowdownResultsMap = new ShowdownResultsMap();
@@ -37,7 +55,14 @@ public class Main {
             thread.join();
         }
 
+        timer.cancel();
+
         System.out.println("Permutations: " + permutations.get());
         System.out.println("Showdown results:\n" + globalShowdownResultsMap);
+
+        //print the time it took to run
+        double endTime = System.currentTimeMillis();
+        double totalTime = endTime - startTime;
+        System.out.println("\nTotal time: " + totalTime + "ms");
     }
 }
